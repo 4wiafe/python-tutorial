a0aae52b6821a8ac543bfe314fb0d59428222ffd
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-03-06 20:34:03 +0000

    Claculate final ticket price

--- movie-ticket-booking-calculator/app.py
+++ movie-ticket-booking-calculator/app.py
@@ -45,5 +45,8 @@ if age >= 21 or age >= 18 and show_time != 'Evening':
     else:
         service_charges = 1
     print('Service charges:', service_charges)
+
+    final_price = base_price + extra_charges + service_charges - discount
+    print('Final price of ticket:', final_price)
 else:
     print('Ticket booking failed due to restrictions')
