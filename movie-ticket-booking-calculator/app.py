da3fa2ab942bc99d6f64977e31b08f1fa6df6344
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-03-06 16:31:29 +0000

    Add extra charges if it's the weekend

--- movie-ticket-booking-calculator/app.py
+++ movie-ticket-booking-calculator/app.py
@@ -10,15 +10,25 @@ if age >= 21:
 else:
     print('User is not eligible for Evening shows')
 
-is_member = True
+is_member = False
 is_weekend = False
 
 discount = 0
 
-if is_member:
+if is_member and age >= 21:
     discount = 3
     print('User qualifies for membership discount')
 else:
     print('User does not qualify for membership discount')
 
 print('Discount:', discount)
+
+extra_charges = 0
+
+if is_weekend:
+    extra_charges = 2
+    print('Extra charges will be applied')
+else:
+    print('No extra charges will be applied')
+
+print('Extra charges:', extra_charges)
