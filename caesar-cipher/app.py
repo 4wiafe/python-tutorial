189df047eba4019f535683a1a110ba768a624713
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-03-09 22:06:28 +0000

    Added validation checks for shift

--- caesar-cipher/app.py
+++ caesar-cipher/app.py
@@ -1,4 +1,10 @@
 def caesar(text, shift):
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(
