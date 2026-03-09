7aa6031b5fd98cc5cf9e8b55d4b4762156f33b8b
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-03-09 17:52:58 +0000

    Add Caesar cipher encryption logic

--- caesar-cipher/app.py
+++ caesar-cipher/app.py
@@ -1,4 +1,7 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 shift = 5
 shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
-print(shifted_alphabet)
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'
+encrypted_text = text.translate(translation_table)
+print(encrypted_text)
